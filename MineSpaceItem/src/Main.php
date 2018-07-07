337564af39ffa5512d8032fe3bbbfd14a9b51146
DevolperPHP <devolperphp@gmail.com>
2018-07-07 14:56:28 +0300

    Update Main.php

--- MineSpaceItem/src/Main.php
+++ MineSpaceItem/src/Main.php
@@ -1,1 +1,10 @@
+<?php
 
+namespace MineSpaceItem;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\level\Position;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
